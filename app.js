0b80e55ea16df5d94babb233b64b7378bf5a7b00
iryna <iryna.savelieva@evergabe.de>
2025-02-25 16:49:54 +0100

    Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /* app.get('/', (req, res) => {
   res
     .status(200)
@@ -13,7 +15,9 @@ app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
 }); */
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -21,6 +25,25 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
 });
 
 const port = 3000;
