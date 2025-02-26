fe4baf30545b03dc8af96c2fb95f2f56a56a046a
iryna <iryna.savelieva@evergabe.de>
2025-02-26 13:09:16 +0100

    Param Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res
+      .status(404)
+      .json({ status: 'fail', message: 'Invalid ID: Tour not found' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -19,12 +30,6 @@ exports.getTour = (req, res) => {
 
   const id = parseInt(req.params.id);
   const tour = tours.find((t) => t.id === id);
-
-  if (!tour) {
-    return res
-      .status(404)
-      .json({ status: 'fail', message: 'Invalid ID: Tour not found' });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -51,12 +56,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res
-      .status(404)
-      .json({ status: 'fail', message: 'Invalid ID: Tour not found' });
-  }
-
   res.status(200).json({
     status: 'success',
     message: 'Tour updated successfully!',
@@ -65,12 +64,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res
-      .status(404)
-      .json({ status: 'fail', message: 'Invalid ID: Tour not found' });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
