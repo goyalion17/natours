8a797417dde89a38f814a6c788c8a1d8707b64df
Iryna Savelieva <irynasavelieva@Mac.local>
2025-03-17 16:40:06 +0100

    Data Validation: Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
     slug: {
       type: String,
@@ -23,10 +25,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     raitingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Raiting must be above 1.0'],
+      max: [5, 'Raiting must be below 5.0'],
     },
     raitingQuantity: {
       type: Number,
