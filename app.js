8ebd256a6001c5d1e565b09413370b35a655b28d
Iryna Savelieva <irynasavelieva@Mac.local>
2025-03-20 08:31:35 +0100

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 🤗');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,8 +22,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
 });
 
 // 4) START SERVER
