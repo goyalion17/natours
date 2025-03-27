0f6d8453cb79a9cbdd5f912777422e3cc7a0c4ff
Iryna Savelieva <irynasavelieva@Mac.local>
2025-03-27 09:06:14 +0100

    Creating New Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,12 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require('./../controllers/userController');
+} = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
