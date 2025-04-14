ddc9f9e15bbb72063b6e976050ba845c73b01d1a
Iryna Savelieva <irynasavelieva@Mac.local>
2025-04-14 08:44:34 +0200

    Password Reset Functionality: Setting New Password

--- models/userModel.js
+++ models/userModel.js
@@ -54,6 +54,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword,
@@ -87,7 +94,7 @@ userSchema.methods.createPasswordResetToken = function () {
 
   console.log({ resetToken }, this.passwordResetToken);
 
-  this.passwordResetExpires = Date.now() + 10 + 60 + 1000;
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
   return resetToken;
 };
